a5f0d69b87d48057cbd959a018c987a0ab422086
a15355447898a <a15355447898a@outlook.com>
2024-04-21 13:50:59 +0800

    设置透明背景

---
+++ lua/plugins/ui-transparent.lua
@@ -0,0 +1,13 @@
+return {
+  -- 背景透明
+  {
+    "tokyonight.nvim",
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    }
+  }
+}
